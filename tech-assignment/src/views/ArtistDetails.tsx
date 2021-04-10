42e13e4e5de57b09d39329a8af81b822af23d92c
ziyi886 <767576197@qq.com>
2021-04-10 02:21:09 -0400

    bind apis

--- tech-assignment/src/views/ArtistDetails.tsx
+++ tech-assignment/src/views/ArtistDetails.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const ArtistDetails = () => {
+    const [albums, setAlbums] = useState<any>([]);
+    const bearer = 'Bearer ' + 'BQB5-cH6ECsfb37WJBQv-u_L_5ysY-IP8FT0A6t5oIsl6ozW54ZKGgHjZBd-EJZlVky576Aq4DZlItwnzgA-gmzPmygU-JgF482H6gPLEIqvLTRQWms1G4uc6HLWsx6ntYVz0w';
+    useEffect(()=>{
+        fetch('https://api.spotify.com/v1/artists/74NBPbyyftqJ4SpDZ4c1Ed/albums',
+        {
+            method: 'GET',
+            headers: {
+                'Authorization': bearer,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then((data) => {
+            setAlbums(data.items);
+        })
+        .catch(console.log)
+    },[])
 
     return (
-        <div>artists details</div>
+        <div>
+            artists details
+            {
+                albums.map((album: any) =>(
+                    <div>{album.name}</div>
+                ))
+            }
+        </div>
     )
 }
--- tech-assignment/src/views/Artists.tsx
+++ tech-assignment/src/views/Artists.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Artists = () => {
+    const [artists, setArtists] = useState<any>([]);
+    const bearer = 'Bearer ' + 'BQB5-cH6ECsfb37WJBQv-u_L_5ysY-IP8FT0A6t5oIsl6ozW54ZKGgHjZBd-EJZlVky576Aq4DZlItwnzgA-gmzPmygU-JgF482H6gPLEIqvLTRQWms1G4uc6HLWsx6ntYVz0w';
+    useEffect(()=>{
+        fetch('https://api.spotify.com/v1/search?q=tom&type=artist',
+        {
+            method: 'GET',
+            headers: {
+                'Authorization': bearer,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then((data) => {
+          setArtists(data.artists.items);
+        })
+        .catch(console.log)
+    },[])
 
     return (
-        <div>artists list</div>
+        <div>
+            artists list
+            {
+                artists.map((artist: any) =>(
+                    <div>{artist.name}</div>
+                ))
+            }
+        </div>
     )
 }
